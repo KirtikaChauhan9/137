29b20e16a8103b709f7322161faf72c1eb204e5b
Kirtika Chauhan <73385205+KirtikaChauhan9@users.noreply.github.com>
2021-04-12 15:31:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+objects = [];
+status = "";
+
+function setup() {
+  canvas = createCanvas(250, 200);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(250,);
+  video.hide();
+}
+
+function preload() {
+   
+
+}
+
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
+
+function start()
+{
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  object_name = document.getElementById("object_name").value;
+}
+function draw() {
+    image(video, 0, 0, 380, 275);
+
+    if (status != "") {
+      objectDetector.detect(video, gotResult);
+      for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Objects Detected";
+          document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
+          fill("red");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          nofill();
+          stroke("red");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }
+  }
+}
+
+
